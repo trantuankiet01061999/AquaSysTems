9fd5dd939242a9d057f572a9b6587d9b19bd5cec
TranTuanKiet <kiet.trantuan@aquavietnam.vn>
2026-02-25 10:59:06 +0700

    fix_table target

--- AquaSolution.Data/Migrations/20260225035147_update_table_target.Designer.cs
+++ AquaSolution.Data/Migrations/20260225035147_update_table_target.Designer.cs
@@ -4,6 +4,7 @@ using AquaSolution.Data.Connection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AquaSolution.Data.Migrations
 {
     [DbContext(typeof(AquaDbContext))]
-    partial class AquaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225035147_update_table_target")]
+    partial class update_table_target
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AquaSolution.Data/Migrations/20260225035147_update_table_target.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AquaSolution.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_table_target : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TargetValue",
+                schema: "KPI",
+                table: "tbl_KPIMonthlyTargets",
+                type: "decimal(18,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TargetValue",
+                schema: "KPI",
+                table: "tbl_KPIMonthlyTargets",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)");
+        }
+    }
+}
